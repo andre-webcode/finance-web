7bc837ce757879b740eff02d2422c6acdaf63a3c
Luiz André <luizandre.progr@gmail.com>
2026-08-09 14:39:36 -0300

    feat: connect login to api

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,17 +1,54 @@
 
+"use client"
 
+import { useState } from "react";
 
 
 const Login = () => {
-   
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch("http://localhost:3001/signin",{
+                method:"POST",
+                headers:{
+                    "Content-type":"application/json",
+                },
+                body: JSON.stringify({
+                    email,
+                    password,
+                })
+            })
+            
+            const data = await response.json();
+
+            console.log("status",response.status)
+            console.log("resposta",data)
+
+        } catch (error) {
+            console.log('Erro:',error)
+        }
+    }
 
     return (
         <main>
             <h1>Login</h1>
 
-            <form>
-                <input type="email" placeholder="E-mail"/>
-                <input type="password" placeholder="Senha" />
+            <form onSubmit={handleSubmit}>
+                <input type="email"
+                    placeholder="E-mail"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                />
+
+                <input type="password"
+                    placeholder="Senha"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                />
 
                 <button type="submit">
                     Entrar
